cb2be842594888e984e6a63f401db474949fb098
fangxx3863 <2221048564@qq.com>
2021-07-30 19:53:41 +0800

    Optimize the code
    Improve performance and remove redundancy

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import usb.core
 import usb.util
-import win32gui
 import win32api
 import win32con
-import sys
 import numpy as np
-#from pynput.keyboard import Key, Listener
 from multiprocessing import Process, Value, Array
-#import pynput.keyboard
-#import pynput
 import multiprocessing
 import time
 import os
 import configparser
-#import multiprocessing_win
 
 
 def pause_ctrl(pctr, raww):
@@ -23,13 +17,11 @@ def pause_ctrl(pctr, raww):
         raise ValueError('Device is not found')
     # device is found :-)
     dev.set_configuration()
-    #zzz = 0
     js = 0
     while True:
         raw = dev.read(0x81,64)
         ctrl = int(''.join([str(zz) for zz in raw[1:2]]))
         raww[:] = list(np.array(raw).flatten())
-        #print(raww)
         if ctrl == 39:
             js = js + 1
         elif ctrl == 66:
@@ -42,11 +34,6 @@ def pause_ctrl(pctr, raww):
 
 def mouse_move():
     print("Run MouseMove")
-    #dev = usb.core.find(idVendor=0x0483, idProduct=0x6003)
-    #if dev is None:
-    #    raise ValueError('Device is not found')
-    # device is found :-)
-    #dev.set_configuration()
     zzz = 0
     while True:
         dir = os.getcwd() 
@@ -58,15 +45,12 @@ def mouse_move():
         XDisplacement = float(config['DEFAULT']['XDisplacement'])
         YDisplacement = float(config['DEFAULT']['YDisplacement'])
         while True:
-            #raw = dev.read(0x81,1000)
             raw = raww[:]
             traw = (raw[13:14])
             yaraw = (raw[9:10])
             ybraw = (raw[8:9])
             xaraw = (raw[7:8])
             xbraw = (raw[6:7])
-            #ctrl = int(''.join([str(zz) for zz in raw[1:2]]))
-            #print(raw)
             xa = float(''.join([str(zz) for zz in xaraw]))
             xb = float(''.join([str(zz) for zz in xbraw]))
             ya = float(''.join([str(zz) for zz in yaraw]))
@@ -80,14 +64,11 @@ def mouse_move():
                 win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
                 zzz = 0
 
-            xx=((xa*256+xb)/XProportion)-XDisplacement               #在这里修改映射大小及位移
-            yy=((ya*256+yb)/YProportion)-YDisplacement               #在这里修改映射大小及位移
-            #xx=int(str(xx).split('.')[0] )
-            #yy=int(str(yy).split('.')[0] )
+            xx=((xa*256+xb)/XProportion)-XDisplacement
+            yy=((ya*256+yb)/YProportion)-YDisplacement
             xx=int(xx)
             yy=int(yy)
             print(xx, "/", yy, "/", t, "/", pctr.value)
-            #print(raw)
             #print(raw)
             win32api.SetCursorPos((xx,yy))   
 
